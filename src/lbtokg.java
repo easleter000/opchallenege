6425ddf189c7218c3cb6927768619820cb37edf8
Terry Easley <easleter000@warren.k12.in.us>
2024-08-07 13:44:59 -0400

    U1L1.4

---
+++ src/lbtokg.java
@@ -0,0 +1,7 @@
+public class lbtokg {
+    public static void main(String[] args) {
+        double pounds = 200;
+        double kilogram = (pounds * 0.45359237);
+        System.out.println(kilogram);
+    }
+}
